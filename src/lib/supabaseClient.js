fa8d160239476152ff24a971cfd45777faab4dd6
wolsybl <122503483+wolsybl@users.noreply.github.com>
2025-07-16 21:21:23 -0500

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xhkzriwtqyyjcuhgypdn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhoa3pyaXd0cXl5amN1aGd5cGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0Mzg3OTcsImV4cCI6MjA2MTAxNDc5N30.pj9uKLQs5opfm-geN3sF8TMf7uPFXtbRc0Y8f95CGAw';
+const supabaseUrl = 'your_supabase_url';
+const supabaseAnonKey = 'your_supabase_anon_key';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
